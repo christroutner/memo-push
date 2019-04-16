9c5a60c40f90c62afe88d9d7eb6fcc55b0a0d560
Chris Troutner <chris.troutner@gmail.com>
2019-04-15 20:21:32 -0700

    Added travis file

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+  Publish IPFS hashes to the BCH blockchain using the memo.cash protocol
+*/
+
+'use strict'
+
+async function memoPush() {
+  console.log(`Hello world!`)
+}
+memoPush()
+
+//module.exports = memoPush
