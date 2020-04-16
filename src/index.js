2502a9c37d5456d53eadb2c5424c07b56514a616
Chris Troutner <chris.troutner@gmail.com>
2020-04-15 22:43:09 -0700

    Successfully created member.cash mutli-tx post

--- src/index.js
+++ src/index.js
@@ -102,6 +102,32 @@ class MemoPushCommand extends Command {
         console.log(`Transaction ID: ${txidStr}`)
         console.log(`https://memo.cash/post/${txidStr}`)
         console.log(`https://explorer.bitcoin.com/bch/tx/${txidStr}`)
+
+        await sleep(5000)
+        console.log(" ")
+        console.log(" ")
+
+        const obj1 = {
+          txid: txidStr,
+          msg: "This is the first test reply"
+        }
+        const reply1Txid = await reply(obj1)
+
+        await sleep(5000)
+        console.log(" ")
+        console.log(" ")
+
+        const obj2 = {
+          txid: reply1Txid,
+          msg: "This is the second test reply"
+        }
+        const reply2Txid = await reply(obj2)
+
+        console.log(" ")
+        console.log(`member.cash post:`)
+        console.log(
+          `https://member.cash/index.html#thread?root=${txidStr}&post=${reply2Txid}`
+        )
       }
 
       //this.log(`hello ${name} from ./src/index.js`)
@@ -138,6 +164,106 @@ function findBiggestUtxo(utxos) {
   }
 
   return utxos[largestIndex]
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+// Expects an object as input. The object must have the following properties:
+// obj.txid = Transaction ID of previous memo that is being replied to.
+// obj.msg = The message to include in the reply.
+async function reply(obj) {
+  try {
+    // Create an EC Key Pair from the user-supplied WIF.
+    const ecPair = _this.bchjs.ECPair.fromWIF(WIF)
+
+    // Generate the public address that corresponds to this WIF.
+    const ADDR = _this.bchjs.ECPair.toCashAddress(ecPair)
+    console.log(`Replying with '${obj.msg}' to ${ADDR}`)
+
+    // Pick a UTXO controlled by this address.
+    const u = await _this.bchjs.Blockbook.utxo(ADDR)
+    const utxo = findBiggestUtxo(u)
+
+    // instance of transaction builder
+    const transactionBuilder = new _this.bchjs.TransactionBuilder()
+
+    //const satoshisToSend = SATOSHIS_TO_SEND
+    const originalAmount = utxo.satoshis
+    const vout = utxo.vout
+    const txid = utxo.txid
+
+    // add input with txid and index of vout
+    transactionBuilder.addInput(txid, vout)
+
+    // TODO: Compute the 1 sat/byte fee.
+    const fee = 500
+
+    // Send the same amount - fee.
+    transactionBuilder.addOutput(ADDR, originalAmount - fee)
+
+    const endChg = changeEndian(obj.txid)
+    // console.log(`Original txid: ${obj.txid}`)
+    // console.log(`txid endian swapped: ${endChg}`)
+
+    // Add the memo.cash OP_RETURN to the transaction.
+    const script = [
+      _this.bchjs.Script.opcodes.OP_RETURN,
+      Buffer.from("6d03", "hex"),
+      Buffer.from(endChg, "hex"),
+      Buffer.from(`REPLY ${obj.msg}`)
+    ]
+
+    //console.log(`script: ${util.inspect(script)}`);
+    const data = _this.bchjs.Script.encode(script)
+    //console.log(`data: ${util.inspect(data)}`);
+    transactionBuilder.addOutput(data, 0)
+
+    // Sign the transaction with the HD node.
+    let redeemScript
+    transactionBuilder.sign(
+      0,
+      ecPair,
+      redeemScript,
+      transactionBuilder.hashTypes.SIGHASH_ALL,
+      originalAmount
+    )
+
+    // build tx
+    const tx = transactionBuilder.build()
+    // output rawhex
+    const hex = tx.toHex()
+    //console.log(`TX hex: ${hex}`);
+    //console.log(` `);
+
+    // Broadcast transation to the network
+    const txidStr = await _this.bchjs.RawTransactions.sendRawTransaction(hex)
+    console.log(`Transaction ID: ${txidStr}`)
+    console.log(`https://memo.cash/post/${txidStr}`)
+    console.log(`https://explorer.bitcoin.com/bch/tx/${txidStr}`)
+
+    return txidStr
+  } catch (err) {
+    console.error(`Error in reply(): `, err)
+  }
+}
+
+// Change the endianness of a hex string.
+function changeEndian(str) {
+  try {
+    //https://stackoverflow.com/questions/44287769/parsing-a-little-endian-hex-string-to-decimal
+    if (!str) return undefined
+
+    var len = str.length
+    var bigEndianHexString = ""
+    for (var i = 0; i < len / 2; i++)
+      bigEndianHexString += str.substring(len - (i + 1) * 2, len - i * 2)
+
+    return bigEndianHexString
+  } catch (err) {
+    console.error(`Error in changeEndian(): `, err)
+  }
 }
 
 module.exports = MemoPushCommand
